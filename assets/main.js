e871965749cc683a47508ff26c1821e6871806d3
Ville Vuorenmaa <villevuor@gmail.com>
2017-03-10 13:20:37 +0200

    Initial commit

---
+++ assets/main.js
@@ -0,0 +1,54 @@
+document.getElementById('link').addEventListener('keyup', function(event) {
+  if (event.keyCode == 13) {
+    event.preventDefault();
+    shortenLink();
+  }
+});
+
+function shortenLink() {
+  var result = document.getElementById('result');
+  var label = document.getElementById('label');
+  var target = document.getElementById('shortened');
+
+  result.classList.remove('active');
+  label.classList.remove('error');
+  document.getElementById('shortened').innerHTML = 'Kopioi linkki';
+
+  var eventId = getEventId(document.getElementById('link').value);
+
+  if ( eventId === false ) {
+    label.classList += ' error';
+    return false;
+  }
+
+  target.href = 'https://kuksaan.fi/' + eventId;
+  target.innerHTML = 'kuksaan.fi/' + eventId;
+
+  setTimeout(function(){
+    result.classList += ' active';
+  }, 100);
+}
+
+function getEventId(link) {
+  var isKuksaLink = /kuksa\.partio\.fi\/Kotisivut\/login\.aspx/i;
+
+  if ( !isKuksaLink.test(link) ) {
+    return false;
+  }
+
+  var getId = /login\.aspx\?Id=([0-9]*)/i;
+
+  return link.match(getId)[1];
+
+  // https://kuksa.partio.fi/Kotisivut/login.aspx?Id=13858
+}
+
+var clipboard = new Clipboard('#copypaste', {
+  text: function() {
+      return document.getElementById('shortened').getAttribute('href').replace('https://', '');
+  }
+});
+
+clipboard.on('success', function(e) {
+  document.getElementById('copypaste').innerHTML = 'Kopioitu!';
+});
